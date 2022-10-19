3f8fed7ae7ad643064118b8fee0d373d36d70263
Lillush <lillush88@gmail.com+>
2022-10-19 20:59:23 +0200

    kész

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 // Importáld be a HumanHero és TransformerHero osztályokat.
-
+import { HumanHero } from "./hero";
+import { TransformerHero } from "./transformer";
 
 /**
  * Állítsd be helyesen a változót!
@@ -8,7 +9,11 @@
  * Hozz létre benne 3 HumanHero példányt, szabadon választott adatokkal.
  * @var {HumanHero[]} humans
  */
-export const humans: any[] = [];
+export const humans: HumanHero[] = [
+  { id: 1, name: "Beni", sex: "male", age: 18, health: 80 },
+  { id: 2, name: "Baba", sex: "female", age: 16, health: 90 },
+  { id: 3, name: "Boszi", sex: "female", age: 17, health: 100 },
+];
 
 /**
  * Állítsd be helyesen a változót!
@@ -17,4 +22,8 @@ export const humans: any[] = [];
  * Hozz létre benne 3 TransformerHero példányt, szabadon választott adatokkal.
  * @var {TransformerHero[]} transformers
  */
-export const transformers: any[] = [];
+export const transformers: TransformerHero[] = [
+  { id: 1, name: "Hiszti", wings: 2, wheels: 0, clan: "Bird" },
+  { id: 2, name: "Bangyi", wings: 4, wheels: 2, clan: "Szitakötő" },
+  { id: 3, name: "Szende", wings: 0, wheels: 4, clan: "Cat" },
+];
